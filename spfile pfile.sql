8e713a5bfc8b177dccbdfc2f76daf686cd89b42f
vamshireddy257 <130881198+vamshireddy257@users.noreply.github.com>
2023-10-08 11:42:47 +0530

    Update spfile pfile.sql

--- spfile pfile.sql
+++ spfile pfile.sql
@@ -26,10 +26,10 @@ SELECT  name,issys_modifiable m_INSTANCE, isses_modifiable m_SESSION  FROM V$par
 
 ---
 SELECT  name,
-case when issys_modifiable = 'FALSE' then 'STATIC'
-when issys_modifiable =  'IMMEDIATE' then 'DYNAMIC' end m_INSTANCE, 
-case when isses_modifiable = 'FALSE' then 'STATIC'
-when isses_modifiable = 'TRUE' then 'DYNAMIC' end  m_SESSION  FROM V$parameter order by 1;
+case when issys_modifiable = 'FALSE' then 'STATIC_INSTANCE'
+when issys_modifiable =  'IMMEDIATE' then 'DYNAMIC_INSTANCE' end m_INSTANCE, 
+case when isses_modifiable = 'FALSE' then 'STATIC_WRT_SESSION'
+when isses_modifiable = 'TRUE' then 'DYNAMIC_WRT_SESSION' end  m_SESSION  FROM V$parameter order by 1;
 ---
 
 /u01/app/oracle/oradata/PRODDB/controlfile/o1_mf_k25kxfxt_.ctl, /u01/app/oracle/fast_recovery_area/PRODDB/controlfile/o1_mf_k25kxfy9_.ctl,/
