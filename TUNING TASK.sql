e03c9490ce28463f26479bc9743dd4f7095b2ce9
vamshireddy257 <130881198+vamshireddy257@users.noreply.github.com>
2024-05-10 10:41:21 +0530

    Update TUNING TASK.sql

--- TUNING TASK.sql
+++ TUNING TASK.sql
@@ -10,26 +10,25 @@ BETWEEN 10000 AND 15000;
 SET AUTOTRACE TRACEONLY;
 
 SELECT COUNT(*) FROM SOE.ORDERS2 WHERE ORDER_TOTAL < 10050;
-
+get sql id from v$sql
 
 SET AUTOT OFF
-DECLARE
-V_TASK_NAME VARCHAR2(30);
-V_SQLTEXT CLOB;
-BEGIN
-V_SQLTEXT := 'select count(*) from student where classno=30';
-V_TASK_NAME := DBMS_SQLTUNE.CREATE_TUNING_TASK (
- SQL_TEXT => V_SQLTEXT,
- USER_NAME => 'SOE',
- SCOPE => 'COMPREHENSIVE',
- TIME_LIMIT => NULL,
- TASK_NAME => 'STA_STUDENT_TTASK',
- DESCRIPTION => 'A sample STA tuning task');
-END;
-/ 
 
-exec DBMS_SQLTUNE.EXECUTE_TUNING_TASK(TASK_NAME=>'STA_STUDENT_TTASK')
-SELECT DBMS_SQLTUNE.REPORT_TUNING_TASK('STA_STUDENT_TTASK' ) FROM DUAL;
+DECLARE
+  l_sql_tune_task_id VARCHAR2(100);
+BEGIN
+  l_sql_tune_task_id := DBMS_SQLTUNE.CREATE_TUNING_TASK(
+                          sql_id => '3h1jhbpk98zwy', 
+                          scope => 'COMPREHENSIVE',
+                          time_limit => 10000,
+                          task_name => 'tuning_task_3h1jhbpk98zwy',
+                          description => 'Tuning task for SQL ID 3h1jhbpk98zwy');
+  DBMS_OUTPUT.PUT_LINE('Task Created: ' || l_sql_tune_task_id);
+END;
+/
+
+exec DBMS_SQLTUNE.EXECUTE_TUNING_TASK(TASK_NAME=>'tuning_task_3h1jhbpk98zwy')
+SELECT DBMS_SQLTUNE.REPORT_TUNING_TASK('tuning_task_3h1jhbpk98zwy' ) FROM DUAL;
 
 
 create index SOE.IDX$$_00350001 on SOE.ORDERS2("ORDER_TOTAL");
