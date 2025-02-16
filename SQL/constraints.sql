acb8088c8678032b3726d77522cecb392d3915c1
vamshireddy257 <130881198+vamshireddy257@users.noreply.github.com>
2025-02-16 09:24:31 +0530

    Update constraints.sql

--- SQL/constraints.sql
+++ SQL/constraints.sql
@@ -14,17 +14,32 @@ create table student
 (sno number(2) unique not null,
 sname varchar2(10),
 dob date,
+doj date
+);
+
+create table student
+(sno number(2) unique not null,
+sname varchar2(10),
+dob date,
 doj date,
 check ( dob < doj ));
 
 insert into student values (1,'A',sysdate-100,sysdate-2);
 insert into student values (2,'B', sysdate-2,sysdate-100);
 
+
+create table student
+(sno number(2) unique not null,
+sname varchar2(10),
+mob_no number(10) check (length(mob_no)=10),
+dob date,
+doj date,
+check ( dob < doj ));
+
 create table dept10
 (deptno number,
 dname varchar2(10),
 check (deptno=10));
-
 
 create table product
 (pid number(2) primary key,
@@ -32,11 +47,11 @@ pname varchar2(10)
 );
 
 
-create table product_list
-(pid number(2),
-quantity number(10),
-price number(6,2),
-foreign key (pid) references product(pid) on delete cascade /set null/set default
+
+create table product
+(pid number(2) ,
+pname varchar2(10),
+primary key(pid)
 );
 
 insert into product values (1,'pen');
@@ -44,6 +59,18 @@ insert into product values (2,'pencil');
 insert into product values (3,'marker');
 
 
-insert into product_list values (1,10,5.00);
+create table invoice
+(pid number(2),
+quantity number(10),
+price number(6,2),
+foreign key (pid) references product(pid) 
+);
 
-insert into product_list values (4,20,200.00);
+on delete cascade /set null/set default
+
+
+
+
+
+insert into invoice values (1,10,5.00);
+insert into invoice values (4,20,200.00);
